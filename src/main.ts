559d75b4daedd7632bcd7a12d7e88f125986d5fb
Yutaka Kamei <kamei@yykamei.me>
2021-06-27 06:34:13 +0900

    test 2

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { setFailed } from "@actions/core"
 import { run } from "./run"
 
 process.on("unhandledRejection", handleError)
+console.log("test")
 run().catch(handleError)
 
 function handleError(err: unknown): void {
