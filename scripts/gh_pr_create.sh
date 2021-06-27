fb417bbf4c77249c1fbb925608a3e5d96d7107f4
Yutaka Kamei <kamei@yykamei.me>
2021-06-28 06:55:04 +0900

    Fix gh_pr_create.sh to use `/tmp`

--- scripts/gh_pr_create.sh
+++ scripts/gh_pr_create.sh
@@ -2,5 +2,5 @@
 
 set -xeuo pipefail
 
-cat > tmp/body.txt
-gh pr create --head "$BRANCH" --title "$TITLE" --label "$LABEL" --body-file tmp/body.txt
+cat > /tmp/body.txt
+gh pr create --head "$BRANCH" --title "$TITLE" --label "$LABEL" --body-file /tmp/body.txt
