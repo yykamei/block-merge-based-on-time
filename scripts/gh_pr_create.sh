751aa3a77a62dbafebc42323156dabf2a4329dd0
Yutaka Kamei <kamei@yykamei.me>
2021-06-28 15:09:31 +0900

    Do nothing if there are no changes (#22)

--- scripts/gh_pr_create.sh
+++ scripts/gh_pr_create.sh
@@ -2,5 +2,10 @@
 
 set -xeuo pipefail
 
+changes=$(git status --short | wc -l)
+if [[ "$changes" == "0" ]]; then
+  exit 0
+fi
+
 cat > /tmp/body.txt
 gh pr create --head "$BRANCH" --title "$TITLE" --label "$LABEL" --body-file /tmp/body.txt
