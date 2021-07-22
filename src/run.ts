4cd040e4df2e5708d2cfd21299180b3c93a5920b
Yutaka Kamei <kamei@yykamei.me>
2021-07-22 20:43:02 +0900

    Test PR

--- src/run.ts
+++ src/run.ts
@@ -7,6 +7,7 @@ import { shouldBlock } from "./should-block"
 
 export async function run(): Promise<void> {
   const inputs = new Inputs()
+  console.log("test")
 
   switch (context.eventName) {
     case "schedule":
