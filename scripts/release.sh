1936837c6b6cb8e90e89aa473b1aee34ccd9e2fa
Yutaka Kamei <kamei@yykamei.me>
2021-07-22 21:28:17 +0900

    Update tags at the same time (#107)

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,6 +6,9 @@ set -xeuo pipefail
 grep -F "## [${FUTURE_RELEASE}]" CHANGELOG.md
 grep -F '## [Unreleased]' CHANGELOG.md && exit 1
 
+MINOR_TAG="${FUTURE_RELEASE%.*}"
+MAJOR_TAG="${FUTURE_RELEASE%%.*}"
+
 # Extract the latest changelog entries to put them into GitHub release notes.
 hit=0
 while IFS="" read -r line; do
@@ -21,6 +24,10 @@ done < CHANGELOG.md
 gh release list
 cat /tmp/notes.txt
 
+git tag --force "$MINOR_TAG"
+git tag --force "$MAJOR_TAG"
+
 if [[ "$APPLY" == "true" ]]; then
   gh release create "$FUTURE_RELEASE" --title "$RELEASE_TITLE" --notes-file /tmp/notes.txt
+  git push --force --tags
 fi
