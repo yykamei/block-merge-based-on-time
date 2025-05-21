be3acd03a055b272132e870b9a084d1106bfa689
Yutaka Kamei <kamei@yykamei.me>
2025-05-21 23:11:47 +0900

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
-import { setFailed } from "@actions/core"
+import { setFailed, error } from "@actions/core"
 import { run } from "./run"
 
 process.on("unhandledRejection", handleError)
 run().catch(handleError)
 
 function handleError(err: unknown): void {
+  error("ERROR!")
+  error(`${err instanceof Error}`)
   if (err instanceof Error) {
     setFailed(`Unhandled error: ${err.message}\n${err.stack}`)
   } else {
